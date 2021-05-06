decb15255c1ec2d26e5c463cad0802543f732e0a
Brian <bdang0003@gmail.com>
2021-05-06 11:57:56 -0700

    created computerPlay() playRound() and game(). Works regardless of case sensitivity. Need to fix: incorrect user input

---
+++ index.js
@@ -0,0 +1,61 @@
+function computerPlay(){  //generates random choice for computer opponent (rock/paper/scissors)
+    switch(Math.floor(Math.random()*3)){
+        case 0: return "Rock"; break;
+        case 1: return "Paper"; break;
+        case 2: return "Scissors"; break;
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    console.log(`player's choice: ${playerSelection} \ncomputer's choice: ${computerSelection}`);
+    switch(playerSelection){
+        case "ROCK":
+            switch(computerSelection){
+                case "Rock": return "It's a tie!"; break;
+                case "Paper": return "You Lose! Paper beats Rock"; break;
+                case "Scissors": return "You Win! Rock beats Scissors!"; break;
+            }
+        case "PAPER":
+            switch(computerSelection){
+                case "Rock": return "You Win! Paper beats Rock!"; break;
+                case "Paper": return "It's a tie!"; break;
+                case "Scissors": return "You Lose! Rock beats Paper!"; break;
+            }
+        case "SCISSORS":
+            switch(computerSelection){
+                case "Rock": return "You Lose! Rock beats Scissors"; break;
+                case "Paper": return "You Win! Scissors beats Paper"; break;
+                case "Scissors": return "It's a tie!"; break;
+            }
+    }
+}
+
+function game(){
+    let i;
+    for(i = 0; i<5; i++){
+        let playerSelection;
+        playerSelection = window.prompt(`Round ${i+1}: Enter Rock, Paper, or Scissors:`).toUpperCase();
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
+
+
+
+
+
+
+
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
+// console.log(computerPlay());
