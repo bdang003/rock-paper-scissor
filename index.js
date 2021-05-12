180472f836aa1f101306910f28acd83fbf0584e2
Brian <bdang0003@gmail.com>
2021-05-12 16:44:05 -0700

    created openEndScreen() that removes all elements from page and then adds the end screen

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+function playRound(){ //compares choice between the player's and computer's choice
+    let playerSelection = this.id.toUpperCase();
+    if(!scoreboardCreated){ //creates scoreboard if it has not yet been created
+        createScoreBoard();
+    }
+    switch(playerSelection){
+        case "ROCK": //situation: player chooses "Rock"
+            switch(computerChoice()){ //compares player choice to computer choice and then updates score/display
+                case "Rock": updateScore('Tied!'); break;
+                case "Paper": computerWinsQuantity++; updateScore('Round lost! Paper beats Rock'); break;
+                case "Scissors": playerWinsQuantity++; updateScore('Round won! Rock beats Paper'); break;
+            }
+        case "PAPER": //situation: player chooses "Paper"
+            switch(computerChoice()){ //compares player choice to computer choice and then updates score/display
+                case "Rock": playerWinsQuantity++; updateScore('Round won! Paper beats Rock'); break;
+                case "Paper": updateScore('Tied!'); break;
+                case "Scissors": computerWinsQuantity++; updateScore('Round Lost! Scissors beats Paper'); break;
+            }
+        case "SCISSORS": //situation: player chooses "Scissors"
+            switch(computerChoice()){ //compares player choice to computer choice and then updates score/display
+                case "Rock": computerWinsQuantity++; updateScore('Round lost! Rock beats Scissors'); break;
+                case "Paper": playerWinsQuantity++; updateScore('Round won! Scissors beats Rock'); break;
+                case "Scissors": updateScore('Tied!'); break;
+            }
+    }
+    if(computerWinsQuantity === 5 || playerWinsQuantity === 5){
+        openEndScreen();
+    }
+}
+
 function computerChoice(){  //generates random choice for computer opponent (rock/paper/scissors)
     switch(Math.floor(Math.random()*3)){
         case 0: return "Rock"; break;
@@ -6,34 +36,7 @@ function computerChoice(){  //generates random choice for computer opponent (roc
     }
 }
 
-function playRound(){ //compares choice between the player's and computer's choice
-    let playerSelection = this.id.toUpperCase();
-    if(!scoreboardCreated){
-        createScoreBoard();
-    }
-    switch(playerSelection){
-        case "ROCK": //situation: player chooses "Rock"
-            switch(computerChoice()){ //compares player choice to computer choice
-                case "Rock": updateScore('Tied'); break;
-                case "Paper": computerWinsQuantity++; updateScore('Round lost! Paper beats Rock'); break;
-                case "Scissors": playerWinsQuantity++; updateScore('Round won! Rock beats Paper'); break;
-            }
-        case "PAPER": //situation: player chooses "Paper"
-            switch(computerChoice()){ //compares player choice to computer choice
-                case "Rock": playerWinsQuantity++; updateScore('Round won! Paper beats Rock'); break;
-                case "Paper": updateScore('Tied'); break;
-                case "Scissors": computerWinsQuantity++; updateScore('Round Lost! Scissors beats Paper'); break;
-            }
-        case "SCISSORS": //situation: player chooses "Scissors"
-            switch(computerChoice()){ //compares player choice to computer choice
-                case "Rock": computerWinsQuantity++; updateScore('Round lost! Rock beats Scissors'); break;
-                case "Paper": playerWinsQuantity++; updateScore('Round won! Scissors beats Rock'); break;
-                case "Scissors": updateScore('Tied'); break;
-            }
-    }
-}
-
-function createScoreBoard(){
+function createScoreBoard(){ //creates container with text content
     const container = document.querySelector('#main');
     const div = document.createElement('div');
     const h1 = document.createElement('h1');
@@ -52,9 +55,19 @@ function createScoreBoard(){
     scoreboardCreated = true;
 }
 
-function updateScore(playRecap){
+function updateScore(playRecap){ //updates scores to reflect changes
     document.getElementById('playRecapDisplay').innerHTML = playRecap;
     document.getElementById('scoreboardDisplay').innerHTML = `Player: ${playerWinsQuantity} Computer ${computerWinsQuantity}`;
+}
+
+function openEndScreen(){
+    const main = document.querySelector('#main');
+    while(main.firstChild){
+        main.removeChild(main.lastChild);
+    }
+    const endScreen = document.createElement('div');
+    endScreen.innerHTML = "THIS IS THE END";
+    main.appendChild(endScreen);
 }
 
 //global variables declared
@@ -63,4 +76,3 @@ let computerWinsQuantity = 0;
 let scoreboardCreated = false;
 const choices = document.querySelectorAll('.choice');
 choices.forEach(choice => choice.addEventListener('click', playRound));
-
