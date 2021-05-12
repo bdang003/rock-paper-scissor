e5a85c79631b944e40a2b8fa669e2bf7e94f5d5c
Brian <bdang0003@gmail.com>
2021-05-12 15:34:42 -0700

    created createScoreboard() and updateScore()

--- index.js
+++ index.js
@@ -7,28 +7,51 @@ function computerChoice(){  //generates random choice for computer opponent (roc
 }
 
 function playRound(){ //compares choice between the player's and computer's choice
-    let playerSelection = this.id;
+    let playerSelection = this.id.toUpperCase();
+    if(!scoreboardCreated){
+        createScoreBoard();
+    }
     console.log(`player's choice: ${playerSelection} \ncomputer's choice: ${computerChoice()}`);
     switch(playerSelection){
         case "ROCK": //situation: player chooses "Rock"
             switch(computerChoice()){ //compares player choice to computer choice
                 case "Rock": return "It's a tie!"; break;
-                case "Paper": computerWinsQuantity++; return "You Lose! Paper beats Rock"; break;
-                case "Scissors": playerWinsQuantity++; return "You Win! Rock beats Scissors!"; break;
+                case "Paper": computerWinsQuantity++; updateScore(); break;
+                case "Scissors": playerWinsQuantity++; updateScore(); return "You Win! Rock beats Scissors!"; break;
             }
         case "PAPER": //situation: player chooses "Paper"
             switch(computerChoice()){ //compares player choice to computer choice
-                case "Rock": playerWinsQuantity++; return "You Win! Paper beats Rock!"; break;
+                case "Rock": playerWinsQuantity++; updateScore(); break;
                 case "Paper": return "It's a tie!"; break;
-                case "Scissors": computerWinsQuantity++; return "You Lose! Rock beats Paper!"; break;
+                case "Scissors": computerWinsQuantity++; updateScore(); break;
             }
         case "SCISSORS": //situation: player chooses "Scissors"
             switch(computerChoice()){ //compares player choice to computer choice
-                case "Rock": computerWinsQuantity++; return "You Lose! Rock beats Scissors"; break;
-                case "Paper": playerWinsQuantity++; return "You Win! Scissors beats Paper"; break;
+                case "Rock": computerWinsQuantity++; updateScore(); break;
+                case "Paper": playerWinsQuantity++; updateScore(); break;
                 case "Scissors": return "It's a tie!"; break;
             }
     }
+}
+
+function createScoreBoard(){
+    const container = document.querySelector('#main');
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+    div.id = "scoreboardContainer";
+    h1.id = "scoreboardHeader";
+    p.id = "scoreboardDisplay";
+    h1.textContent="Current Score:\n";
+    p.textContent=`Player: ${playerWinsQuantity} Computer: ${computerWinsQuantity}`;
+    div.appendChild(h1);
+    div.appendChild(p);
+    container.appendChild(div);
+    scoreboardCreated = true;
+}
+
+function updateScore(){
+    document.getElementById('scoreboardDisplay').innerHTML = `Player: ${playerWinsQuantity} Computer ${computerWinsQuantity}`;
 }
 
 function declareWinner(){ //displays alert box with winner based on final score
@@ -38,8 +61,9 @@ function declareWinner(){ //displays alert box with winner based on final score
 }
 
 //global variables declared
-let playerWinsQuantity;
-let computerWinsQuantity;
+let playerWinsQuantity = 0;
+let computerWinsQuantity = 0;
+let scoreboardCreated = false;
 const choices = document.querySelectorAll('.choice');
 choices.forEach(choice => choice.addEventListener('click', playRound));
 
